ba9c065314c90b662b85bb718661f8480d807221
Hugo Pierre de Oliveira Faria <hugo.pierre12255@gmail.com>
2024-09-01 23:10:26 -0300

    Add files via upload

--- supermercado.py
+++ supermercado.py
@@ -9,6 +9,19 @@ from tkinter import messagebox
 produtos = []
 carrinho = []
 
+frame_loja = None
+canvas_loja = None
+scrollbar_loja = None
+entrada_filtro = None
+entrada_categoria = None
+
+frame_compras = None
+canvas_compras = None
+scrollbar_compras = None
+
+nova_janela = None
+nova_janela_agenda = None
+
 def carregar_produtos():
     global produtos
     try:
@@ -35,20 +48,23 @@ def placeholder_entry(entry, placeholder_text):
 
 
 def add_loja():
-    global frame_loja, canvas, scrollbar, entrada_filtro, entrada_categoria
+    global frame_loja, canvas_loja, scrollbar_loja, entrada_filtro, entrada_categoria
 
     if frame_loja is not None and frame_loja.winfo_ismapped():
         frame_loja.place_forget()
     else:
         if frame_loja is None:
-            frame_loja = tk.Frame(app, width=300, height=80)
-            canvas = tk.Canvas(frame_loja, width=300, height=80)
-            scrollbar = ttk.Scrollbar(frame_loja, orient="vertical", command=canvas.yview)
-            scrollbar.pack(side="right", fill="y")
-            canvas.pack(side="left", fill="both", expand=True)
-            canvas.create_window((0, 0), window=frame_loja, anchor="nw")
-            canvas.configure(yscrollcommand=scrollbar.set)
-            frame_loja.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+            # Cria um frame para a loja e um canvas dentro dele
+            frame_loja = tk.Frame(app, width=300, height=300)
+            canvas_loja = tk.Canvas(frame_loja, width=300, height=300)
+            scrollbar_loja = ttk.Scrollbar(frame_loja, orient="vertical", command=canvas_loja.yview)
+            
+            scrollbar_loja.pack(side="right", fill="y")
+            canvas_loja.pack(side="left", fill="both", expand=True)
+            canvas_loja.create_window((0, 0), window=frame_loja, anchor="nw")
+            canvas_loja.configure(yscrollcommand=scrollbar_loja.set)
+
+            frame_loja.bind("<Configure>", lambda e: canvas_loja.configure(scrollregion=canvas_loja.bbox("all")))
 
             entrada_filtro = tk.Entry(frame_loja)
             placeholder_entry(entrada_filtro, "Filtrar por nome")
@@ -69,10 +85,7 @@ def add_loja():
         if produtos:
             for produto in produtos:
                 preco = produto.get('preco', 'N/A')
-                if preco == 'N/A':
-                    texto_preco = 'N/A'
-                else:
-                    texto_preco = f"R${preco:.2f}"
+                texto_preco = f"R${preco:.2f}" if preco != 'N/A' else 'N/A'
                 
                 nome = produto.get('nome', 'N/A')
                 codigo = produto.get('codigo', 'N/A')
@@ -85,18 +98,18 @@ def add_loja():
 
         frame_loja.place(x=400, y=120)
 
-
 def aplicar_filtro(event):
     filtro_nome = entrada_filtro.get().lower()
     filtro_categoria = entrada_categoria.get().lower()
-    
-    for botao in frame_loja.winfo_children():
-        if isinstance(botao, ttk.Button):
-            texto = botao.cget("text").lower()
-            if filtro_nome in texto and filtro_categoria in texto:
-                botao.pack()
+
+    for widget in frame_loja.winfo_children():
+        if isinstance(widget, ttk.Button):
+            texto = widget.cget("text").lower()
+
+            if (filtro_nome in texto or filtro_nome == '') and (filtro_categoria in texto or filtro_categoria == ''):
+                widget.pack(pady=5)
             else:
-                botao.pack_forget()
+                widget.pack_forget()
 
 def adicionar_ao_carrinho(codigo, nome, preco):
     global carrinho
@@ -110,12 +123,58 @@ def adicionar_ao_carrinho(codigo, nome, preco):
         json.dump(carrinho, arquivo, indent=4)
 
     atualizar_carrinho()
+    
+    messagebox.showinfo(f"Sucesso", "Seu produto foi para o carrinho!")
 
 def salvar_carrinho():
     with open('carrinho.json', 'w', encoding='utf-8') as arquivo:
         json.dump(carrinho, arquivo, indent=4)
 nova_janela = None
 
+
+def carregar_dados():
+    with open('saida.json', 'r') as file:
+        dados = json.load(file)
+    
+    contador = {}
+    for item in dados:
+        codigo = item['codigo']
+        if codigo in contador:
+            contador[codigo] += 1
+        else:
+            contador[codigo] = 1
+    
+    return contador
+
+def mostrar_compras():
+    global frame_compras, canvas_compras, scrollbar_compras
+
+    if frame_compras is not None and frame_compras.winfo_ismapped():
+        frame_compras.pack_forget()
+    else:
+        if frame_compras is None:
+            frame_compras = tk.Frame(app)
+            canvas_compras = tk.Canvas(frame_compras, width=300, height=300)
+            scrollbar_compras = ttk.Scrollbar(frame_compras, orient="vertical", command=canvas_compras.yview)
+            
+            scrollbar_compras.pack(side="right", fill="y")
+            canvas_compras.pack(side="left", fill="both", expand=True)
+            canvas_compras.create_window((0, 0), window=frame_compras, anchor="nw")
+            canvas_compras.configure(yscrollcommand=scrollbar_compras.set)
+
+            frame_compras.bind("<Configure>", lambda e: canvas_compras.configure(scrollregion=canvas_compras.bbox("all")))
+
+            frame_compras.place(x=300, y=760)
+
+        for widget in frame_compras.winfo_children():
+            widget.destroy()
+
+        contador = carregar_dados()
+
+        for codigo, quantidade in contador.items():
+            label = tk.Label(frame_compras, text=f"Produto {codigo}: {quantidade} vezes")
+            label.pack(pady=5)
+            
 def atualizar_carrinho():
     global nova_janela, carrinho
     if nova_janela is not None and nova_janela.winfo_ismapped():
@@ -142,7 +201,7 @@ def atualizar_carrinho():
         label_total = tk.Label(nova_janela, text=f"Total: R${total_preco:.2f}")
         label_total.grid(row=len(carrinho), column=0, pady=10)
 
-        botaoc = ttk.Button(nova_janela, text="Finalizar Compra", command=finalizar_compra)
+        botaoc = ttk.Button(nova_janela, text="Finalizar Compra", command=finalizar_carrinho)
         botaoc.grid(row=len(carrinho) + 1, column=0, pady=10)
         botaod = ttk.Button(nova_janela, text="Apagar carrinho", command=remover_produto_02)
         botaod.grid(row=len(carrinho) + 2, column=0, pady=10)
@@ -187,12 +246,12 @@ def remover_produto_02():
     largura_janela = janela_confirmacao.winfo_reqwidth()
     altura_janela = janela_confirmacao.winfo_reqheight()
 
-    x = (largura_tela - largura_janela) // 2
+    x = (largura_tela - largura_janela) // 10
     y = (altura_tela - altura_janela) // 2
 
     janela_confirmacao.geometry(f"{largura_janela}x{altura_janela}+{x}+{y}")
 
-    label = tk.Label(janela_confirmacao, text="Você tem certeza que deseja\nremover esse produto?")
+    label = tk.Label(janela_confirmacao, text="Você tem certeza que deseja\nremover todos os\nprodutos do carrinho?")
     label.place(x=20, y=20)
         
     label2 = tk.Button(janela_confirmacao, text="Sim", command=lambda: confirmar_remocao_02())
@@ -233,9 +292,37 @@ def ver_carrinho():
         nova_janela.place(x=900, y=90)
 
     atualizar_carrinho()
+    
+def finalizar_carrinho():
+    global label2, janela_confirmacao, index_produto_remover
+
+    janela_confirmacao = tk.Toplevel()
+    janela_confirmacao.title("Cuidado!")
+
+    janela_confirmacao.update_idletasks()
+
+    largura_tela = janela_confirmacao.winfo_screenwidth()
+    altura_tela = janela_confirmacao.winfo_screenheight()
+    largura_janela = janela_confirmacao.winfo_reqwidth()
+    altura_janela = janela_confirmacao.winfo_reqheight()
+
+    x = (largura_tela - largura_janela) // 2
+    y = (altura_tela - altura_janela) // 2
+
+    janela_confirmacao.geometry(f"{largura_janela}x{altura_janela}+{x}+{y}")
+
+    label = tk.Label(janela_confirmacao, text="Você tem certeza que deseja\nfinalizar a compra?")
+    label.place(x=20, y=20)
+    
+    label2 = tk.Button(janela_confirmacao, text="Sim", command=lambda: finalizar_compra())
+    label2.place(x=100, y=70)
+    
+    label3 = tk.Button(janela_confirmacao, text="Cancelar", command=cancelar_remocao)
+    label3.place(x=130, y=70)
 
 def finalizar_compra():
     global produtos, carrinho
+    messagebox.showinfo("Sucesso", "Sua compra foi finalizada com sucesso!\nObrigado por comprar no mercado sabe muito.")
     ver_carrinho()
     
     try:
@@ -248,7 +335,6 @@ def finalizar_compra():
 
     with open('saida.json', 'w', encoding='utf-8') as arquivo_saida:
         json.dump(saida, arquivo_saida, indent=4)
-    recarregar_produtos()
 
     for item in carrinho:
         codigo_produto = item['codigo']
@@ -263,23 +349,46 @@ def finalizar_compra():
 
     carrinho = []
     atualizar_carrinho()
-
-def recarregar_produtos():
-    global produtos
-    messagebox.showinfo(f"Sucesso", "seus produtos foram para a sua loja!")
-    carregar_produtos()
+    
+    messagebox
 
 def open_agenda_app():
     global nova_janela_agenda
+
+    with open('produto.json', 'r', encoding='utf-8') as arquivo:
+        try:
+            data = json.load(arquivo)
+            produtos = data.get("produtos", [])
+        except json.JSONDecodeError:
+            messagebox.showerror("Erro", "Erro ao carregar o arquivo JSON.")
+            return
+
+    produtos_em_falta = []
+
+    for item in produtos:
+        if isinstance(item, dict):
+            if item.get("quantidade", 0) < 30:
+                nome = item.get("nome", "Produto Desconhecido")
+                quantidade = item.get("quantidade", 0)
+                produtos_em_falta.append(f"{nome}: {quantidade}")
+        else:
+            messagebox.showerror("Erro", "Formato de produto inválido no arquivo JSON.")
+            return
+
+    if produtos_em_falta:
+        mensagem = "ESTES PRODUTOS ESTÃO EM FALTA NO SEU ESTOQUE E DEVEM SER REPOSTOS COM URGÊNCIA!:\n"
+        mensagem += "\n".join(produtos_em_falta)
+        messagebox.showinfo("ATENÇÃO!", mensagem)
+
     if nova_janela_agenda is not None and nova_janela_agenda.winfo_exists():
-        nova_janela_agenda.destroy()
-        nova_janela_agenda = None
+        nova_janela_agenda.lift()
     else:
         nova_janela_agenda = tk.Toplevel(app)
         nova_janela_agenda.geometry("395x547+50+100")
         nova_janela_agenda.title("Agenda App")
-        agenda = AgendaApp(nova_janela_agenda)
 
+        agenda = AgendaApp(nova_janela_agenda)
+            
 app = ttk.Window(themename="darkly")
 app.title("Supermercado")
 app.state('zoomed') 
@@ -302,8 +411,8 @@ label.grid(row=0, column=0, padx=10, pady=10)
 botao1 = ttk.Button(app, text="Estoque", command= lambda: open_agenda_app())
 botao1.place(x=20, y=30)
 
-botao1 = ttk.Button(app, text="Saída de\nprodutos")
-botao1.place(x=1000, y=30)
+botao1 = ttk.Button(app, text="Saída de\nprodutos", command=mostrar_compras)
+botao1.place(x=250, y=700)
 
 botao_add_loja = ttk.Button(app, text="abrir Loja", command=add_loja)
 botao_add_loja.place(x=600, y=70)
